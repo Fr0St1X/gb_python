2b8e6332cdb11fafe196818955f9aaff72563360
Fr0St1X <97782761+Fr0St1X@users.noreply.github.com>
2023-02-15 13:44:13 +0300

    изменено на выборку по списку, а не символу

--- zad13.py
+++ zad13.py
@@ -2,13 +2,26 @@
 # ufs = User's First String
 # uss = User's First String
 
-ufs = str(input('Введите первую строку: ')) # Первая строка ввода пользователя
-uss = str(input('Введите вторую строку: ')) # Вторая строка ввода пользователя
+# Строки ввода пользователя
+ufs = str(input('Введите первую строку: ')) 
+uss = str(input('Введите вторую строку: '))
+
+# Удаление символов в строках
+ufs = ufs.replace('.', '').replace(',', '').replace('-', '').replace('+', '').replace('!', '').replace('?', '') 
+uss = uss.replace('.', '').replace(',', '').replace('-', '').replace('+', '').replace('!', '').replace('?', '') 
+
+# Перевод строк в нижний регистр для упрощения выборки
+ufs = ufs.lower() 
+uss = uss.lower()
+
+# Преобразование строк в списки
+ufs_list = ufs.split()
+print(ufs_list)
+uss_list = uss.split()
 
 samechars = 0 # Переменная одинаковых символов
-
-for chars in ufs: # Для символов в певой строке
-    if chars in uss: # Сравнить с символами во второй строке
+for chars in ufs_list: # Для символов в первой строке
+    if chars in uss_list: # Сравнить с символами во второй строке
         samechars += 1 # Счетчик одинаковых символов, и передача их количества (через + 1) в переменную samechars
 
 print(f'Количество вхождений {samechars}') #Вывод
